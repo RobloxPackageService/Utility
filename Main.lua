14aed27abfd9092aad0aacf5b4f0a30bf10d8348
Brad Sharp <BradSharp@users.noreply.github.com>
2017-08-09 17:16:44 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,20 @@
-return {
-  Create = require 'Create',
-  Signal = require 'Signal'
-}
+local module = {}
+
+function module.Create(name, parent)
+   return function(props)
+      local new = Instance.new(name)
+      for i, v in next, props do
+         if type(i) == 'number' then
+            v.Parent = new
+         else
+            new[i] = v
+         end
+      end
+      new.Parent = new.Parent or parent
+      return new
+   end
+end
+
+module.Signal = require 'Signal'
+
+return module
